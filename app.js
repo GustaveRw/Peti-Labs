cd30f22197e64c8709448ce8be453b7d8cfddc2c
Jean Marie Gustave MBONYINSHUTI <giggygustave.gg@gmail.com>
2022-11-23 16:58:46 +0200

    other features changed

--- app.js
+++ app.js
@@ -9,3 +9,29 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+function sendMail(){
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  
+const serviceID = 'service_41marok'
+const templateID = 'template_uy9yla1'
+
+emailjs.send(serviceID, templateID, params)
+.then(
+  res => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    
+
+    console.log(res)
+    alert("Your message was sent succesfully")
+  })
+  .catch((err) => console.log(err));
+
+}
